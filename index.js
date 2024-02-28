dbb2d77f3867362903afe98960dffacd713ed8d8
Kipperfeito <heitor.k.w@gmail.com>
2024-02-28 09:22:15 -0300

    Soma

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ import express from 'express'
 const app = express()
 const port = 3000
 app.set('view engine', 'ejs')
-
+app.use(express.urlencoded({extended: true}))
 app.listen(port)
 
 app.get('/', (req, res) => {
     res.render("index")})
-
-
-app.get('/soma/:valor1/:valor2', (req, res) => {
-    var valor1 = req.params.valor1
-    var valor2 = req.params.valor2
-    var soma = parseInt(valor1) + parseInt(valor2)
     
-    res.send("O resultado da soma é: "+ soma)
+app.get('/soma', (req, res) => {
+    res.render('soma')
+})
+
+app.post('/calculadora', (req, res) => {
+    res.send("O resultado da soma é: "+ (parseInt(req.body.valor1) + parseInt(req.body.valor2)))
+})
+
+app.post('/pesquisar', (req, res) => {
+    res.send("Dados recebidos: "+ req.body.nome)
 })
