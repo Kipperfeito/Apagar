942e9c2a667d7cfe9cf1ae4c80fd096b4a4506ee
Kipperfeito <heitor.k.w@gmail.com>
2024-03-27 09:20:20 -0300

    Edit

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,62 +1,63 @@
 import Usuario from '../modelos/usuario.js'
 
-export function abriupload(req,res){
+export function abriupload(req, res) {
     res.render('upload')
 }
 
-export function upload(req,res) {
-    res.send("<img src='/"+req.file.filename+"'>")
+export function upload(req, res) {
+    res.send("<img src='/" + req.file.filename + "'>")
 }
-export function hello (req, res) {
-    res.send("Hello")}
+export function hello(req, res) {
+    res.send("Hello")
+}
 
-export function abrecalculadora (req, res) {
+export function abrecalculadora(req, res) {
     res.render('calculadora')
 }
 
-export function calculadora (req, res) {
+export function calculadora(req, res) {
     const op = req.body.op;
     var v1 = parseInt(req.body.valor1)
     var v2 = parseInt(req.body.valor2)
     let resultado;
-     if(op == 'Soma'){
-         resultado = (v1 + v2)
-     }
-     else if(op == 'Subtração'){
+    if (op == 'Soma') {
+        resultado = (v1 + v2)
+    }
+    else if (op == 'Subtração') {
         resultado = (v1 - v2)
-     }
-     else if(op == 'Multiplicação'){
-         resultado = (v1 * v2)
-     }
-     else if (op == 'Divisão') {
-         resultado = (v1 / v2)
-     }
-     res.render('calculadora', {op, resultado})
- }
+    }
+    else if (op == 'Multiplicação') {
+        resultado = (v1 * v2)
+    }
+    else if (op == 'Divisão') {
+        resultado = (v1 / v2)
+    }
+    res.render('calculadora', { op, resultado })
+}
 
-export function abretabela (req, res) {
+export function abretabela(req, res) {
     res.render('tabela')
 }
 
-export function tabela (req, res) {
+export function tabela(req, res) {
     var valor = parseInt(req.body.linhas)
-    res.render('tabela', {linhas:valor, colunas:req.body.colunas})
+    res.render('tabela', { linhas: valor, colunas: req.body.colunas })
 }
 
-export function pesquisa (req, res) {
+export function pesquisa(req, res) {
     res.render('index')
 }
 
-export function pesquisado (req, res) {
-    res.send("Dados Recebidos de "+ req.body.nome)
+export function pesquisado(req, res) {
+    res.send("Dados Recebidos de " + req.body.nome)
 }
 
-export function abretela (req, res) {
+export function abretela(req, res) {
     res.render('usuario')
 }
 
-export async function mostradados (req, res) {
-    const usuario = new Usuario ({
+export async function cadastro(req, res) {
+    const usuario = new Usuario({
         nome: req.params.nome,
         email: req.params.email,
         senha: req.params.senha,
@@ -67,4 +68,26 @@ export async function mostradados (req, res) {
     console.log(usuario);
 
     await usuario.save();
+    res.render('usuario')
+}
+export async function mostrausuarios(req, res) {
+    let usuarios = await Usuario.find({})
+    res.render('mostrausuarios', { Usuarios: usuarios })
+}
+export async function buscarusuarios(req, res) {
+    let usuarios = await Usuario.find({ nome: req.body.pesquisar })
+    res.render('mostrausuarios', { Usuarios: usuarios })
+}
+export async function abretelaeditar(req, res) {
+    let usuario = await Usuario.findById(req.params.id);
+    res.render('editarusuario', { Usuario: usuario });
+}
+export async function editarusuario(req, res) {
+    let usuario = await Usuario.findById(req.params.id)
+    usuario.nome = req.body.nome;
+    usuario.email = req.body.email;
+    usuario.senha = req.body.senha;
+    usuario.datanasc = req.body.datanasc;
+    await usuario.save();
+    res.redirect('/mostrausuarios')
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 const foto = multer({ dest: './public' })
 import express from 'express';
-import {hello, calculadora, abrecalculadora, abretabela, tabela, pesquisa, pesquisado, abriupload, upload, abretela, mostradados} 
-from '../controllers/controller.js'
+import { cadastro, editarusuario, mostrausuarios, abretelaeditar, hello, calculadora, abrecalculadora, abretabela, tabela, pesquisa, pesquisado, abriupload, upload, abretela, buscarusuarios }
+    from '../controllers/controller.js'
 
 const router = express.Router();
 
@@ -16,5 +16,11 @@ router.post('/pesquisa', pesquisado)
 router.get('/upload', abriupload)
 router.post('/upload', foto.single('foto'), upload)
 router.get('/usuario', abretela)
-router.post('/usuario', foto.single('foto'), mostradados)
+router.post('/usuario', foto.single('foto'), cadastro)
+
+router.get('/mostrausuarios', mostrausuarios)
+router.post('/mostrausuarios', buscarusuarios)
+
+router.get('/editarusuario/:id', abretelaeditar)
+router.post('/editarusuario/:id', editarusuario)
 export default router;
